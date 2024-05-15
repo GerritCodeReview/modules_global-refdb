e71513b1a86ce62d2fd6bccdb0440f3563bc049b
Wendy Wang <wendy.wang10@sap.com>
2024-05-15 16:46:54 +0200

    Replace Custom EnforcementRules with storeAllRefs/storeNoRefs
    Default behavior maintained - draft comments, cache-automerge
    refs, and immutable non-meta refs not stored by default.
    
    For projects in `storeAllRefs`, all refs including the above
    will be stored. For projects in `storeNoRefs`, no refs will be
    stored. `storeNoRefs` will be processed before `storeAllRefs`,
    a project matched in both will not be stored.
    
    Example to store all refs for all projects:
    ```
    [ref-database "storeAllRefs"]
      project = *
    ```
    
    Example to store all refs for projects except X and Y:
    ```
    [ref-database "storeAllRefs"]
      project = *
    [ref-database "storeNoRefs"]
      project = X
      project = Y
    ```
    
    This simplifies the code base significantly while fulfilling
    existing use cases.
    
    Change-Id: I33a07f41c6c4fdffc177152da7a253b7b87d9d03

--- src/test/java/com/gerritforge/gerrit/globalrefdb/validation/RefUpdateValidatorTest.java
+++ src/test/java/com/gerritforge/gerrit/globalrefdb/validation/RefUpdateValidatorTest.java
@@ -27,10 +27,11 @@ import static org.mockito.Mockito.when;
 
 import com.gerritforge.gerrit.globalrefdb.GlobalRefDbSystemError;
 import com.gerritforge.gerrit.globalrefdb.validation.RefUpdateValidator.OneParameterFunction;
-import com.gerritforge.gerrit.globalrefdb.validation.dfsrefdb.DefaultSharedRefEnforcement;
 import com.gerritforge.gerrit.globalrefdb.validation.dfsrefdb.RefFixture;
+import com.gerritforge.gerrit.globalrefdb.validation.dfsrefdb.SharedRefEnforcement;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.entities.Project;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
@@ -44,8 +45,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RefUpdateValidatorTest implements RefFixture {
-  private static final DefaultSharedRefEnforcement defaultRefEnforcement =
-      new DefaultSharedRefEnforcement();
+  private static final SharedRefEnforcement defaultRefEnforcement =
+      new SharedRefEnforcement(new SharedRefDbConfiguration(new Config(), "testplugin"));
 
   @Mock SharedRefDatabaseWrapper sharedRefDb;
 
@@ -196,7 +197,6 @@ public class RefUpdateValidatorTest implements RefFixture {
 
     Result result =
         refUpdateValidator.executeRefUpdate(refUpdate, () -> Result.NEW, rollbackFunction);
-
     verify(rollbackFunction, times(1)).invoke(any());
     assertThat(result).isEqualTo(Result.LOCK_FAILURE);
   }
@@ -233,6 +233,7 @@ public class RefUpdateValidatorTest implements RefFixture {
     Result result =
         refUpdateValidator.executeRefUpdate(refUpdate, () -> Result.NEW, rollbackFunction);
     assertEquals(Result.LOCK_FAILURE, result);
+
     verify(rollbackFunction, times(1)).invoke(any());
   }
 
