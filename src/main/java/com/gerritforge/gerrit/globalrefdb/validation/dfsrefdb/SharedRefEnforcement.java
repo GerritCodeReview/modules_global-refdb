adee59cc0dfcc3bc8a4debeec4476763392a5b2e
Wendy Wang <wendy.wang10@sap.com>
2024-05-15 16:46:54 +0200

    Enhance EnforcementRules to selectively store refs
    Couple the storage of refs in global-refdb to EnforcementRules.
    
    Allow refs to be explicitly included or excluded from global-refdb
    storage with wildcard matching. Desirable for use cases where one
    may want to store even immutable refs. This change makes the
    default hardcoded rules overridable.
    
    This change retains the default behavior to ignore draft comments,
    cache-automerge refs, and immutable non-meta refs.
    
    To store all refs in the global-refdb, use `rule = INCLUDE:*:*`
    
    Example configuration to include all draft-comments, exclude all
    multi-site version refs, but follow the remainder of default
    behavior for ref exclusion:
    
    [ref-database "enforcementRules"]
     rule = INCLUDE:*:refs/draft-comments/*
     rule = EXCLUDE:*:refs/multi-site/version/value
    
    Per-project rules also supported:
    
    [ref-database "enforcementRules"]
     rule = INCLUDE:test-project:refs/draft-comments/*
    
    Rules apply in order top-down, such that the first matched rule is
    the one to take effect. Default rules apply after custom rules.
    
    Change-Id: I33a07f41c6c4fdffc177152da7a253b7b87d9d03

--- src/main/java/com/gerritforge/gerrit/globalrefdb/validation/dfsrefdb/SharedRefEnforcement.java
+++ src/main/java/com/gerritforge/gerrit/globalrefdb/validation/dfsrefdb/SharedRefEnforcement.java
@@ -18,9 +18,36 @@ import com.google.gerrit.entities.RefNames;
 
 /** Type of enforcement to implement between the local and shared RefDb. */
 public interface SharedRefEnforcement {
-  public enum EnforcePolicy {
-    IGNORED,
-    REQUIRED;
+  enum Policy {
+    INCLUDE,
+    EXCLUDE;
+  }
+
+  class Rule {
+    private final String projectWildcard;
+    private final String refWildcard;
+    private final Policy policy;
+
+    public Rule(SharedRefEnforcement.Policy policy, String projectWildcard, String refWildcard) {
+      this.policy = policy;
+      this.projectWildcard = projectWildcard;
+      this.refWildcard = refWildcard;
+    }
+
+    public Policy getPolicy() {
+      return this.policy;
+    }
+
+    public boolean matches(String projectName, String refName) {
+      return wildcardMatches(this.projectWildcard, projectName)
+          && wildcardMatches(this.refWildcard, refName);
+    }
+
+    private boolean wildcardMatches(String wildcardPattern, String value) {
+      return wildcardPattern.equals(value)
+          || (wildcardPattern.endsWith("*")
+              && value.startsWith(wildcardPattern.substring(0, wildcardPattern.length() - 1)));
+    }
   }
 
   /**
@@ -28,20 +55,21 @@ public interface SharedRefEnforcement {
    *
    * @param projectName project to be enforced
    * @param refName ref name to be enforced
-   * @return the {@link EnforcePolicy} value
+   * @return the {@link Policy} value
    */
-  public EnforcePolicy getPolicy(String projectName, String refName);
+  public Policy getPolicy(String projectName, String refName);
 
   /**
    * Get the enforcement policy for a project
    *
    * @param projectName the name of the project
-   * @return the {@link EnforcePolicy} value
+   * @return the {@link Policy} value
    */
-  public EnforcePolicy getPolicy(String projectName);
+  public Policy getPolicy(String projectName);
 
   /**
-   * Check if a refName should be ignored by global refdb. The Default behaviour is to ignore:
+   * Check if a refName should be ignored by global refdb. The default behaviour activates after any
+   * custom rules, if no matches for the custom rules are found or no custom rules are provided.
    *
    * <ul>
    *   <li>refs/draft-comments :user-specific temporary storage that does not need to be seen by
