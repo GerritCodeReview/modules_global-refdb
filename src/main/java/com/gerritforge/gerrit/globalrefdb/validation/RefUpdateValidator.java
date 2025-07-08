e27ba918e3a7ffdab1da3018e69bace069ad3c76
Daniele Sassoli <danielesassoli@gmail.com>
2025-07-08 16:05:13 +0100

    Handle null putValue in global refdb comparison
    Previously, it was assumed that, upon ref deletion, a `refPair.putValue`
    would default to the zero object ID when null, but this was not the
    case. Even compareAndPut on L244 has to deal with ref.putValue being 0
    and transforming it to 0s.
    
    Ensure that the sentinel expands a null refPair.putValue to o the Zero
    object ID so that so that it behaves the same on both sides of the
    check.
    
    Change-Id: I5514845a778d8d0a83c8a042a24d3f64297e63b9

--- src/main/java/com/gerritforge/gerrit/globalrefdb/validation/RefUpdateValidator.java
+++ src/main/java/com/gerritforge/gerrit/globalrefdb/validation/RefUpdateValidator.java
@@ -226,7 +226,10 @@ public class RefUpdateValidator {
             Optional.ofNullable(refDb.findRef(refPair.compareRef.getName()))
                 .map(Ref::getObjectId)
                 .orElse(ObjectId.zeroId());
-        if (!localObjectId.equals(refPair.putValue)) {
+        ObjectId candidateValue =
+            Optional.ofNullable(refPair.putValue)
+                .orElse(ObjectId.zeroId());
+        if (!localObjectId.equals(candidateValue)) {
           String error =
               String.format(
                   "Aborting the global-refdb update of %s = %s: local ref value is %s instead of"
